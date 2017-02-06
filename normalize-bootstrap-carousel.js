225314ec000587d90258bc981bd8d2fb0dc528d4
Quinn Supplee <quinn@quinnsupplee.com>
2017-02-05 20:23:37 -0800

    Create normalize-bootstrap-carousel.js

---
+++ normalize-bootstrap-carousel.js
@@ -0,0 +1,33 @@
+	// Normalize Carousel Heights - pass in Bootstrap Carousel items.
+	// see https://coderwall.com/p/uf2pka/normalize-twitter-bootstrap-carousel-slide-heights
+	$.fn.carouselHeights = function() {
+		var items = $(this),
+			//grab all slides
+			heights = [],
+			//create empty array to store height values
+			tallest; //create variable to make note of the tallest slide
+		var normalizeHeights = function() {
+				items.each(function() { //add heights to array
+					heights.push($(this).height());
+				});
+				tallest = Math.max.apply(null, heights); //cache largest value
+				items.each(function() {
+					$(this).css('min-height', tallest + 'px');
+				});
+			};
+		normalizeHeights();
+		$(window).on('resize orientationchange', function() {
+			//reset vars
+			tallest = 0;
+			heights.length = 0;
+			items.each(function() {
+				$(this).css('min-height', '0'); //reset min-height
+			});
+			normalizeHeights(); //run it again 
+		});
+	};
+	jQuery(function($) {
+		$(window).on('load', function() {
+			$('#carousel-example-generic .item').carouselHeights();
+		});
+	});
